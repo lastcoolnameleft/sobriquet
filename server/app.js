d6fc4b9e578976657abd8d718e85f6ce79a529a4
Tommy Falgout <tommy@lastcoolnameleft.com>
2020-05-10 22:07:03 -0500

    removed dead files

--- server/app.js
+++ server/app.js
@@ -4,12 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//var indexRouter = require('./routes/index');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
